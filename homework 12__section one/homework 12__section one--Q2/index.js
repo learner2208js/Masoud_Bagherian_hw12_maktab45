a97bab2ddeeb8e3d1ea135e5b6f7bef36c6b83cb
Pouria Baqerian <pouriacss.69@gmail.com>
2021-01-31 12:30:57 +0330

    Q1--2 done

---
+++ homework 12__section one/homework 12__section one--Q2/index.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+
+http
+  .createServer(function (req, res) {
+    const path = req.url;
+    if (path === '/') {
+      res.write('<h1>hello world!</h1>');
+      res.end();
+    } else if (path === '/users') {
+      fs.readFile('./users/users.json', 'utf-8', function (err, users) {
+        if (err) {
+          res.writeHead(404, {
+            'content-type': 'text/html',
+          });
+          res.write('<h1>users not found</h1>');
+          res.end();
+          return;
+        }
+        res.write(users);
+        res.end();
+      });
+    }
+  })
+  .listen(8000, 'localhost');
