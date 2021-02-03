a53baeb9df0a2c16a00afd60e8188bd71b801ccf
Pouria Baqerian <pouriacss.69@gmail.com>
2021-02-03 19:41:52 +0330

    Q1--1 404 page added

--- homework 12__section one/homework 12__section one--Q1/index.js
+++ homework 12__section one/homework 12__section one--Q1/index.js
@@ -5,6 +5,10 @@ http
     if (req.url === '/') {
       res.write('<h1>hello world!</h1>');
       res.end();
+    } else {
+      res.writeHead(404);
+      res.write('<h1>page not found!</h1>');
+      res.end();
     }
   })
   .listen(8000, 'localhost');
